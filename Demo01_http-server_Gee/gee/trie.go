998d3f907461d7ca6d1418cb77f6c66c49066cc7
Forrest554 <934655044@qq.com>
2022-05-15 22:23:55 +0800

    trie start

---
+++ Demo01_http-server_Gee/gee/trie.go
@@ -0,0 +1,11 @@
+/*
+使用 Trie 树实现动态路由
+*/
+package gee
+
+type node struct {
+	pattern  string  // 待匹配路由，例如 /p/:lang
+	part     string  // 路由中的一部分，例如 :lang
+	children []*node // 子节点，例如 [doc, tutorial, intro]
+	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+}
